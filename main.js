fbd5639d23bf7d58913c2b4e416c14f4d9d4e3fe
limbwal <dmbujak3@gmail.com>
2018-04-08 06:58:55 -0400

    made code more robust, it fade function was just hacked together before

--- main.js
+++ main.js
@@ -1,31 +1,48 @@
-
-
-function fadeInAndOut() {
-    var opacity = 1;
-    var timer;
-    var addVal = -0.01;
+function fadeWelcomeInAndOut(){
+    var element = document.getElementById("welcome");
+    fade(element, 1500, "out", function(){ 
+        element.textContent = "Take a look around.";
+        //element.className = "content" //issue!
+        fade(element, 1500, "in");
+    });
+}
+function fade(element, time, inOrOut, funcToExecuteOnEnd){
+    var opacity = 0;
+    var opacityIncrement = 17 / time ; //number of times it must go to get to 0 in x seconds while doing 60fps
+    if (inOrOut == "out"){
+        opacityIncrement *= -1;
+        opacity = 1;
+    }
     function fade() {
-        opacity += addVal;
-        document.getElementById("welcome").style.opacity = "" + opacity;
+        opacity += opacityIncrement;
+        element.style.opacity = "" + opacity;
         if (opacity <= 0){ 
-            addVal*=-1;
-            document.getElementById("welcome").textContent="Take a look around.";
-            document.getElementById("div1").className = "content2"; //delete original
-        }
-        else if (opacity >= 1) {
+            funcToExecuteOnEnd();
             clearInterval(timer);
-
         }
     }
-    timer = setInterval(fade, 25);
+    let timer = setInterval(fade, 17); //60 fps
 }
-
-function scrollToAbout(){
-    $("html, body").animate({ scrollTop: $("#about-div").offset().top+Number($("#about-div").css("padding-top").substring(0,2))}, 1500); 
-    return true;
-}
-
-window.onload = function () {
-    fadeInAndOut();
+window.onload = function onloadFunc(){
+    fadeWelcomeInAndOut();
 };
 
+function scrollToAbout(){
+   // $("html, body").animate({ scrollTop: $("#about-div").offset().top+Number($("#about-div").css("padding-top").substring(0,2))}, 1500); 
+   
+    acceleratedScroll($("#about-div").offset().top + Number($("#about-div").css("padding-top").substring(0,2)), 1000);
+    return true;
+}
+function acceleratedScroll (target, time){
+    var current = document.documentElement.scrollTop;
+    var diff = target - current;
+    var smoothFactor = 25;
+    var accelerationFactor = diff / smoothFactor;
+    time /= smoothFactor;
+    diff /= smoothFactor;
+    for (var i = 0; i < smoothFactor; i ++){
+        current += diff * accelerationFactor * i;
+        $("html, body").animate({ scrollTop: current}, time);
+    }
+    
+}
